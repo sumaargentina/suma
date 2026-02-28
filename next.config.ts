5c09c35ebb9a9b97d7774062e2f50ca73855b51f
sumaargentina <miapp.suma@gmail.com>
2026-02-28 00:40:59 -0300

    feat: habilitar React Compiler para mejorar el rendimiento

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 });
 
 const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
